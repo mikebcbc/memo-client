0888a5e8d32c2f9406ecfee80fe635ed24d41e0c
mikebcbc <mikebconstantino@gmail.com>
2017-12-04 15:45:56 -0500

    Hydrate authtoken from localstorage

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
 
 import Header from '../Header/Header';
 import Stats from '../Stats/Stats';
 import Content from '../Content/Content';
 import ManualEntry from '../ManualEntry/ManualEntry';
 
-export default class Dashboard extends Component {
+export class Dashboard extends Component {
   render() {
+    if(!this.props.loggedIn) {
+      return <Redirect to="/login" />;
+    }
     return (
       <div className="dashboard">
         <Header />
@@ -17,3 +22,9 @@ export default class Dashboard extends Component {
     )
   }
 }
+
+const mapStateToProps = state => ({
+  loggedIn: state.memo.authToken !== null
+});
+
+export default connect(mapStateToProps)(Dashboard);
