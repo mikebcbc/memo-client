9aa1cb678e9386c305a5e08105285baad657f95e
mikebcbc <mikebconstantino@gmail.com>
2017-12-04 15:51:06 -0500

    Login/Logout in header

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+
+import {setCurrentUser, setAuthToken} from '../../actions';
+import {clearAuthToken} from '../../local-storage';
 
 import './Nav.css';
 
-export default class Nav extends Component {
+export class Nav extends Component {
+  logOut() {
+  	this.props.dispatch(setCurrentUser(null));
+  	this.props.dispatch(setAuthToken(null));
+  	clearAuthToken();
+  }
+
   render() {
     return (
     	<nav>
     		<ul>
     			<li className="dashboard-link"><Link to="/dashboard">Dashboard</Link></li>
+          {
+            (this.props.loggedIn)
+              ? <li><Link to="/" onClick={() => this.logOut()}>Logout</Link></li>
+              : <li><Link to="/login">Login</Link></li>
+          }
     		</ul>
     	</nav>
     )
   }
-}
+};
+
+const mapStateToProps = state => ({
+	loggedIn: state.memo.authToken !== null
+});
+
+export default connect(mapStateToProps)(Nav);
